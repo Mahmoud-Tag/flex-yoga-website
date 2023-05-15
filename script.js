e5d84a174463b3ea4cb6d850712a4bb4d178987f
Mahmoud Tag <55888427+Mahmoud-Tag@users.noreply.github.com>
2023-05-15 05:43:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ gsap.fromTo(
   { opacity: 1 },
   { opacity: 0, duration: 1.5, delay: 3.5 }
 );
+
+// hidden loading-div after 6 sec 
+const myTimeout = setTimeout(loading, 6000);
+
+function loading() {
+  document.getElementById("loading").style.display = "none";
+}
